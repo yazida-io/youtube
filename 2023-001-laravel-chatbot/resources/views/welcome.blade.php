0f9fcafc5de5c6f50cae125c9874753f5b601a46
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-02-04 13:31:49 +0100

    2023-003-chatbot-2

--- 2023-001-laravel-chatbot/resources/views/welcome.blade.php
+++ 2023-001-laravel-chatbot/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
 <body>
 <main class="h-screen w-screen bg-stone-200 flex justify-center items-center">
-    <div class="container bg-stone-100 gap-4 max-w-4xl h-[700px] rounded-xl p-4 flex justify-start flex-col">
+    <div class="container gap-4 max-w-4xl h-[700px] rounded-xl p-4 flex justify-start flex-col">
         <livewire:chat-bot />
     </div>
 </main>
