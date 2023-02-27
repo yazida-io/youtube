7a66e0d148306ee68600577087ede56c444e91bc
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-02-27 21:30:25 +0100

    2023-011-installation-vuejs

---
+++ 2023-011-installation-vuejs/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+main {
+  height: 100vh;
+  width: 100vw;
+  background-color: antiquewhite;
+}
+</style>
