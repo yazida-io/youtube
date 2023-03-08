25c21dfa5eb6ca0892684bbbe0debd8549fd09ba
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-03-08 22:34:37 +0100

    2023-016-events

---
+++ 2023-016-events/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Search from "./components/Search.vue";
+
+const onSearch = (value: string) => {
+  console.log("input:", value);
+};
+</script>
+
+<template>
+  <main>
+    <search @submit="onSearch" />
+  </main>
+</template>
---
+++ 2023-016-events/src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+    <input type="text" v-model="input" placeholder="type somthing..." />
+    <button @click="onSubmit">Search</button>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const emit = defineEmits<{
+    (e: 'submit', value: string): void;
+}>();
+
+const input = ref<string>('');
+const onSubmit = () => {
+    emit('submit', 'input');
+};
+</script>
