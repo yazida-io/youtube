b164a9f67c79996c5cbe9b22b42a109297313e34
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-03-09 23:23:38 +0100

    2023-018-input-and-data-binding

---
+++ 2023-018-input-and-data-binding/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import AddItem from "./components/AddItem.vue";
+import InputDataBinding from "./components/InputDataBinding.vue";
+
+const items = ref<string[]>([]);
+
+const onAddItem = (input: string) => {
+  items.value.push(input);
+};
+</script>
+
+<template>
+  <main>
+    <!-- <add-item @submit-input="onAddItem" />
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li>
+    </ul> -->
+    <input-data-binding />
+  </main>
+</template>
---
+++ 2023-018-input-and-data-binding/src/components/AddItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="input"
+      placeholder="type somthing..."
+      @keyup.enter="submit"
+    />
+    <button @click="submit">ADD</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits<{
+  (e: "submitInput", value: string): void;
+}>();
+
+const input = ref("");
+const submit = () => {
+  emit("submitInput", input.value);
+  input.value = "";
+};
+</script>
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
