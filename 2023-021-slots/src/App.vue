0be6ae2b50ada34f07aa24f509a1c3e96e9cea64
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-03-13 23:34:11 +0100

    2023-021-slots

---
+++ 2023-021-slots/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Layout from "./components/Layout.vue";
+
+const currentPage = ref("Home");
+
+const onUpdate = ({ link }) => {
+  currentPage.value = link;
+};
+</script>
+
+<template>
+  <layout @update="onUpdate">
+    <p>
+      {{ currentPage }}
+    </p>
+
+    <template #footer>
+      <p>loremksdwhfklsjdflk</p>
+    </template>
+  </layout>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  line-height: normal;
+  text-transform: uppercase;
+}
+</style>
