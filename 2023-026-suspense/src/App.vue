768227e13871d823d87a1566700875456bfd8f8b
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-03-19 23:46:59 +0100

    2023-026-suspense

---
+++ 2023-026-suspense/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>🍃 Suspense</h1>
+    <suspense>
+      <async-component />
+
+      <template #fallback>
+        <h2>loading...</h2>
+      </template>
+    </suspense>
+  </main>
+</template>
+<script setup>
+import AsyncComponent from "./components/AsyncComponent.vue";
+</script>
+
+<style scoped>
+main {
+  overflow-y: scroll;
+}
+</style>
