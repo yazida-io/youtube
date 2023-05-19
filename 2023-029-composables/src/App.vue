fff86f653de7ba51065b820a86b7c547f7eb5bb3
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-05-19 08:10:16 +0200

    2023-029-composables

---
+++ 2023-029-composables/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <composable />
+  </main>
+</template>
+<script>
+import Composable from "./components/Composable.vue";
+
+export default {
+  components: {
+    Composable,
+  },
+  created() {
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+main {
+  overflow-y: scroll;
+}
+</style>
