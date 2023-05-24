fff4e31d7f4658ac8708415b2c8416eb992f52b6
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-05-24 23:01:39 +0200

    2023-036-pinia-introduction

---
+++ 2023-036-pinia-introduction/src/components/CountComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import counterStore from "@/stores/counterStore";
+
+const store = counterStore()
+
+const increment = () => store.count++
+</script>
+
+<template>
+    <button @click="increment">Increment {{ store.count }}</button>
+</template>
