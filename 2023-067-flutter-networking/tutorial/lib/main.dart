a5e8f83ef630d919a05a2e97e3a6497daada4d45
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-06-26 20:55:27 +0200

    2023-067-flutter-networking

---
+++ 2023-067-flutter-networking/tutorial/lib/main.dart
@@ -0,0 +1,119 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyTutorialApp());
+}
+
+class MyTutorialApp extends StatefulWidget {
+  const MyTutorialApp({super.key});
+
+  @override
+  State<MyTutorialApp> createState() => _MyTutorialAppState();
+}
+
+class _MyTutorialAppState extends State<MyTutorialApp> {
+  late Future<List<Product>> futureProduct;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProduct = fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Networking & APIs',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.deepPurpleAccent,
+                    Colors.deepOrange,
+                  ],
+                ),
+              ),
+              child: FutureBuilder(
+                future: futureProduct,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(snapshot.error.toString()),
+                    );
+                  }
+
+                  if (snapshot.hasData) {
+                    return Center(
+                      child: ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                children: [
+                                  Text("${snapshot.data![index].price}"),
+                                  Text(snapshot.data![index].title),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  }
+
+                  return const Center(child: CircularProgressIndicator());
+                },
+              )),
+        ),
+      ),
+    );
+  }
+
+  Future<List<Product>> fetchProducts() async {
+    var response =
+        await http.get(Uri.parse("https://fakestoreapi.com/products"));
+
+    if (response.statusCode == 200) {
+      List document = jsonDecode(response.body);
+      return document.map((product) => Product.fromJson(product)).toList();
+    }
+
+    throw Exception("Une erreur est survenue");
+  }
+}
+
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      price: double.parse("${json['price']}"),
+    );
+  }
+}
